c1b5d9a1487eca427692727aff694389790d9b3f
christiantadam <63576449+christiantadam@users.noreply.github.com>
2023-06-17 09:55:38 +0700

    Update home.blade.php
    coba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-md-10 RDZMobilePaddingLR0">
-                <h1 style="text-align: center">HOME</h1>
+                <h1 style="text-align: center">HOME Main</h1>
                 <div class="acs-grid-container">
 
                 </div>
