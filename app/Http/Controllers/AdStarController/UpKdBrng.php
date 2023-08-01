cf434ffb470f6b190dbc3713681cf9fff54d190e
MaroEnverico <104429235+MaroEnverico@users.noreply.github.com>
2023-08-01 08:47:53 +0700

    db upkdbarang

--- app/Http/Controllers/AdStarController/UpKdBrng.php
+++ app/Http/Controllers/AdStarController/UpKdBrng.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\AdStarController;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
 
 class UpKdBrng extends Controller
 {
     public function index()
     {
-        return view('AdStar.UpKdBrng');//
+        $dataUpKdBrng = DB::connection('ConnADSTAR')->select('exec SP_1486_ADSTAR_LIST_KODE_BRG ?', [1],[1],NULL,[2]);
+        // dd($dataUpKdBrng);
+        return view('AdStar.UpKdBrng', compact('dataUpKdBrng'));//
     }
 }
--- resources/views/AdStar/UpKdBrng.blade.php
+++ resources/views/AdStar/UpKdBrng.blade.php
@@ -1,77 +1,65 @@
 @extends('layouts.appAdStar')
 @section('content')
+    <script>
+        $(document).ready(function() {
+        $('#tabel_Barang').DataTable({
+            order: [[0, 'asc']]
+        });
+    });
+    </script>
+    <h2>Maint Kode Barang</h2>
 
-<h2>Maint Kode Barang</h2>
+    <div class="body">
+        <div class="card">
+            <div class="table-responsive">
+                <table id="tabel_Barang" class="table table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Nama Barang</th>
+                            <th>Kode Barang</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-<div class="body">
-    <div class="card">
-        <div class="table-container">
-            <table>
-              <thead>
-                <tr>
-                  <th>Nama Barang</th>
-                  <th>Kode Barang</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                  // Simulasi pengambilan data dari database
-                  $data = array(
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    array("Value 1", "Value 2"),
-                    // Tambahkan data lainnya dari database
-                  );
+                        @foreach ($dataUpKdBrng as $data)
+                            <tr>
+                                <td>{{ $data->Nama_brg }}</td>
+                                <td>{{ $data->kd_brg }}</td>
 
-                  // Iterasi melalui data dan buat baris tabel
-                  foreach ($data as $row) {
-                    echo "<tr>";
-                    echo "<td>".$row[0]."</td>";
-                    echo "<td>".$row[1]."</td>";
-                    echo "</tr>";
-                  }
-                ?>
-              </tbody>
-            </table>
-          </div>
-    </div>
 
-    <div class="card">
-        <div class="input-container">
-            <label for="nama-barang">Nama Barang:</label>
-            <input type="text" id="nama-barang" required>
-            <input type="text" id="input1">
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
 
-        <div class="input-container">
-            <label for="kd-barang">Kode Barang:</label>
-            <input type="text" id="kd-barang" required>
-            <input type="text" id="input2">
+        <div class="card">
+            <div class="input-container">
+                <label for="nama-barang">Nama Barang:</label>
+                <input type="text" id="nama-barang" required>
+                <input type="text" id="input1">
+            </div>
+
+            <div class="input-container">
+                <label for="kd-barang">Kode Barang:</label>
+                <input type="text" id="kd-barang" required>
+                <input type="text" id="input2">
+            </div>
+
+            <div class="input-container">
+                <button type="button">List Data</button>
+            </div>
         </div>
 
-        <div class="input-container">
-            <button type="button">List Data</button>
+        <div class="button-container">
+            <button class="add">Add</button>
+            <button class="update">Update</button>
+            <button class="del">Delete</button>
+        </div>
+
+        <div class="scrollable-container">
+            <!-- Add content here -->
         </div>
     </div>
-
-    <div class="button-container">
-        <button class="add">Add</button>
-        <button class="update">Update</button>
-        <button class="del">Delete</button>
-    </div>
-
-    <div class="scrollable-container">
-        <!-- Add content here -->
-    </div>
-</div>
-
 @endsection
