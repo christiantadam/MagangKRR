337a7d0dc207535de83fb4de89806d5b1021a613
albert1911 <albertvalentino19@gmail.com>
2023-10-14 12:57:09 +0700

    Membersihkan web.php

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,6 @@ use App\Http\Controllers\Extruder\ExtruderNet\OrderController;
 use App\Http\Controllers\Extruder\ExtruderNet\PencatatanController;
 use App\Http\Controllers\Extruder\WarehouseTerima\WarehouseController;
 
-/**
- * LAST : KirimGudang() | FrmKirimGelondongan.vb
- *
- * User_id = 4384
- *
- * Dokumentasi Alur Kerja Form + Test Case terhadap database lokal
- * Bisa juga dipakai sebagai User Manual
- * https://docs.google.com/document/d/1dRO-LCljmRm9tND8NfYJJTtf_j88ZXvakePGIBnaaJA/edit?usp=sharing (Extruder.Net)
- * https://docs.google.com/document/d/1vxQOqKXhgnv6ZkSTaBGbSREuw_SvHyHKCJ64Teosxrw/edit?usp=sharing (Berat Komposisi 2)
- */
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
